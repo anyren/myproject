0570c057047662f53799ece05031e383ff3c1b5b
Amanda Nyren <anyren42@gmail.com>
2011-03-21 18:55:19 -0500

    initial commit of django app project from pystar workshop

---
+++ polls/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+import datetime
+
+class Poll(models.Model):
+    def was_published_today(self):
+        return self.pub_date.date() == datetime.date.today()
+    def __unicode__(self):
+        return self.question
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+
+class Choice(models.Model):
+    def __unicode__(self):
+        return self.choice
+    poll = models.ForeignKey(Poll)
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField()
